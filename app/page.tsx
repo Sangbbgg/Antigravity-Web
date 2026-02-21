9fd9aad5f0cb3a909e635ff63e0144cdf8bd4dd5
Sangbbgg <151603886+Sangbbgg@users.noreply.github.com>
2026-02-22 03:25:10 +0900

    chore: absolute zero reset - remove every single file and restart

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-    return (
-        <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-            hello
-        </div>
-    );
+    return <div>hello</div>;
 }
